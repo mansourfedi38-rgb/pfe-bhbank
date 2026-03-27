42a335bdb6c4658bf338f1e2d68408295976797f
hediii64 <medhedibousnina01@gmail.com>
2026-03-27 12:23:58 +0100

    Frontend setup + routing + pages created

--- frontend/bh-bank-energy-ui/src/app/app.routes.ts
+++ frontend/bh-bank-energy-ui/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login';
+import { DashboardComponent } from './pages/dashboard/dashboard';
+import { SensorsComponent } from './pages/sensors/sensors';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'sensors', component: SensorsComponent },
+]
---
+++ frontend/bh-bank-energy-ui/src/app/pages/sensors/sensors.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-sensors',
+  standalone: true,
+  imports: [],
+  templateUrl: './sensors.html',
+  styleUrl: './sensors.scss'
+})
+export class SensorsComponent {
+
+}
