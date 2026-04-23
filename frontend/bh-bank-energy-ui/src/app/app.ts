758702ed03e7bde095dbbab8fca62e4a50e05521
hediii64 <medhedibousnina01@gmail.com>
2026-04-23 11:33:00 +0200

    add footer + fix UI layout

--- frontend/bh-bank-energy-ui/src/app/app.ts
+++ frontend/bh-bank-energy-ui/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   title = 'bh-bank-energy-ui';
+
+  constructor(private router: Router) {}
+
+  get shouldShowFooter(): boolean {
+    return !this.router.url.startsWith('/login');
+  }
 }
---
+++ frontend/bh-bank-energy-ui/src/app/shared/footer/footer.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {
+  readonly socialLinks = [
+    {
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/company/bh-bank',
+      iconClass: 'fa-brands fa-linkedin-in',
+    },
+    {
+      name: 'Instagram',
+      href: 'https://www.instagram.com/bh_bank/',
+      iconClass: 'fa-brands fa-instagram',
+    },
+    {
+      name: 'Facebook',
+      href: 'https://www.facebook.com/BHBank',
+      iconClass: 'fa-brands fa-facebook-f',
+    },
+    {
+      name: 'YouTube',
+      href: 'https://www.youtube.com/channel/UCRw2rFa-9RhCg7Mq1q4Q7Sg',
+      iconClass: 'fa-brands fa-youtube',
+    },
+  ];
+}
